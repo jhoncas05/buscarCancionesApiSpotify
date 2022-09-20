f4ab3033832928e16050a845a70ab255ff5d6f2e
407 <407@academicos.cesde.edu.co>
2022-09-20 17:41:12 -0500

    se actualiza codigo

---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,8 @@
+const TOKEN = "Bearer BQCZUuzHpCH6sqMikCyWmj10wS5h6qKtzpIzm7qZtsE2iVfvqB2nAI-Gz82DgvNLgC9CII4Y9eVm76uCs8STicLygNGbI4PUY0IMH8PSnvHunaP0-13SdB5jrnl0FBntz_UJ0KiXc4QVnjlXFNs61XpYwGHMXU7zSQowy4vbJG79Dt9_eq9pjtewI8D5DCI"
+
+const PETICION = {
+    method: "GET",
+    headers: { Authorization: TOKEN }
+  }
+
+  
